d033b6cfa5dfd2c4f341a4e608b1a84a3cf0ac28
solaleh81 <solalehalizadeh81.com>
2023-09-24 19:38:53 +0330

    replace urls with correct url for accounts.api.v1.yrls.

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -5,5 +5,5 @@ app_name = "accounts"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
-    path("api/v1/", include("blog.api.v1.urls")),
+    path("api/v1/", include("accounts.api.v1.urls")),
 ]
