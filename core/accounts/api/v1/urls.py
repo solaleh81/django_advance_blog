54681483186be21f3fa375f483bc86240b496a56
solaleh81 <solalehalizadeh81.com>
2023-09-24 19:41:48 +0330

    replace urls with correct url for accounts.api.v1.yrls.

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -2,8 +2,6 @@ from django.urls import path, include
 from . import views
 
 
-app_name = "api-v1"
-
 urlpatterns = [
     # registration
     path("registration/", views.RegistrationApiView.as_view(), "registration")
