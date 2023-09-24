1047d1dd19083035d4f4f316624dfa00d22f2ccf
solaleh81 <solalehalizadeh81.com>
2023-09-24 18:55:43 +0330

    adding registration url for accounts.api.v1.

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
+from . import views
 
 
 app_name = "api-v1"
 
-urlpatterns = []
+urlpatterns = [
+    # registration
+    path("registration/", views.RegistrationApiView.as_view(), "registration")
+    # change password
+    # reset password
+    # login token
+    # login JWT
+]
