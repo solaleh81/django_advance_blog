87486cef4e9cc170ed26422c4950a1cc84ec9286
solaleh81 <solalehalizadeh81.com>
2023-09-24 18:46:02 +0330

    adding root urls for accounts/urls.py.

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 app_name = "accounts"
 
 urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
     path("api/v1/", include("blog.api.v1.urls")),
 ]
