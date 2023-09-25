e688f29fd9c55c0c73f93070ae22c89a8f7d7504
solaleh81 <solalehalizadeh81.com>
2023-09-25 13:03:56 +0330

    adding validate def for RegistrationSerializer class.

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -5,4 +5,9 @@ from ...models import User
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["email", "password"]
+        fields = ["email", "password", "password1"]
+
+    def validate(self, attrs):
+        if attrs.get("password") != attrs.get("password1"):
+            raise serializers.ValidationError({"detail": "password doesnt match!"})
+        return super().validate(attrs)
