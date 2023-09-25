b4ad5dab6d5598abee44fd56eeedc185a1d0b174
solaleh81 <solalehalizadeh81.com>
2023-09-25 15:45:59 +0330

    adding create def to RegistrationSerializer class.

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -1,3 +1,4 @@
+from logging import exception
 from rest_framework import serializers
 from ...models import User
 
@@ -12,4 +13,9 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         if attrs.get("password") != attrs.get("password1"):
             raise serializers.ValidationError({"detail": "password doesnt match!"})
+
         return super().validate(attrs)
+
+    def create(self, validated_data):
+        validated_data.pop("password1", None)
+        return super().create(validated_data)
