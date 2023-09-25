42b92c514c629516b91eb4537d32a1eec54d0c6d
solaleh81 <solalehalizadeh81.com>
2023-09-25 15:45:06 +0330

    commenting data and add serializer.data to return Response.

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -11,6 +11,6 @@ class RegistrationApiView(generics.GenericAPIView):
         serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            data = {"email": serializer.validated_data["email"]}
-            return Response(data, status=status.HTTP_201_CREATED)
+            # data = {"email": serializer.validated_data["email"]}
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
