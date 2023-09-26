f1aae3e8bbaa8c6b9b391b3cc72b28bf7b5ee6cf
solaleh81 <solalehalizadeh81.com>
2023-09-26 21:51:12 +0330

    adding token-login url.

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from . import views
 
-
 urlpatterns = [
     # registration
-    path("registration/", views.RegistrationApiView.as_view(), name="registration")
+    path("registration/", views.RegistrationApiView.as_view(), name="registration"),
+    path("token/login/", views.CustomObtainTokenView.as_view(), name="token-login")
     # change password
     # reset password
     # login token
