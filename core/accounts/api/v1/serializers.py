13ca6ecabf0a2aa0a86447dff0e796ab9e44b21e
solaleh81 <solalehalizadeh81.com>
2023-09-29 00:34:30 +0330

    adding read_only fields to ProfileApiSerializer class.

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -86,8 +86,9 @@ class ChangePasswordSerializer(serializers.Serializer):
 
 
 class ProfileApiSerializer(serializers.ModelSerializer):
-    email = serializers.CharField(source="user.email")
+    email = serializers.CharField(source="user.email", read_only=True)
 
     class Meta:
         model = Profile
         fields = ("user", "email", "first_name", "last_name", "image", "description")
+        read_only_fields = ["email"]
